03af83dc3c3bb36775d2f6447474088914abe54c
Ashwani1330 <ashwanihabri@gmail.com>
2024-06-29 14:21:24 +0530

    DeleteAllTasksDialog preview added

--- app/src/main/java/com/example/tasky/utils/DeleteAllTasksDialog.kt
+++ app/src/main/java/com/example/tasky/utils/DeleteAllTasksDialog.kt
@@ -7,6 +7,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.tasky.view_models.TaskViewModel
 
 @Composable
@@ -44,4 +47,13 @@ fun MutableState<Boolean>.deleteAllTasksDialog(
         )
     }
     return this
+}
+
+@Preview
+@Composable
+fun PreviewDeleteAllTasksDialog() {
+    val showDialog = remember { mutableStateOf(true) }
+    val viewModel = TaskViewModel()
+
+    showDialog.deleteAllTasksDialog(viewModel)
 }
